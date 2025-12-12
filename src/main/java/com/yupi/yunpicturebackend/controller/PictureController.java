d20d95353234baee3e411a81bf7221260c62bc8c
shine_world <18272214732@163.com>
2025-12-12 17:47:59 +0800

    多级缓存

--- src/main/java/com/yupi/yunpicturebackend/controller/PictureController.java
+++ src/main/java/com/yupi/yunpicturebackend/controller/PictureController.java
@@ -3,6 +3,8 @@ package com.yupi.yunpicturebackend.controller;
 import cn.hutool.core.util.RandomUtil;
 import cn.hutool.json.JSONUtil;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.github.benmanes.caffeine.cache.Cache;
+import com.github.benmanes.caffeine.cache.Caffeine;
 import com.yupi.yunpicturebackend.annotation.AuthCheck;
 import com.yupi.yunpicturebackend.common.BaseResponse;
 import com.yupi.yunpicturebackend.common.DeleteRequest;
@@ -20,19 +22,20 @@ import com.yupi.yunpicturebackend.model.vo.PictureVO;
 import com.yupi.yunpicturebackend.service.PictureService;
 import com.yupi.yunpicturebackend.service.UserService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.data.redis.core.ValueOperations;
 import org.springframework.util.DigestUtils;
-import org.springframework.beans.BeanUtils;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
+
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
+import java.time.Duration;
 import java.util.Arrays;
 import java.util.Date;
 import java.util.List;
-import java.util.concurrent.TimeUnit;
 
 @RestController
 @RequestMapping("/picture")
@@ -46,6 +49,15 @@ public class PictureController {
     private PictureService pictureService;
     @Autowired
     private StringRedisTemplate stringRedisTemplate;
+
+    /**
+     * 构造本地缓存
+     */
+    private final Cache<String, String> LOCAL_CACHE = Caffeine.newBuilder()
+            .initialCapacity(1024)
+            .maximumSize(10_000L)//最大10000条
+            .expireAfterWrite(Duration.ofMinutes(5))  // 缓存 5 分钟移除
+                    .build();
 
     /**
      * 上传图片（可重新上传）
@@ -218,24 +230,35 @@ public class PictureController {
         //构建缓存key
         String queryCondition = JSONUtil.toJsonStr(pictureQueryRequest);
         String hashKey = DigestUtils.md5DigestAsHex(queryCondition.getBytes());
-        String redisKey = String.format("yupicture:listPictureVOByPage:%s", hashKey);
-        //操作Redis, 从缓存中查询
-        ValueOperations<String, String> opsForValue = stringRedisTemplate.opsForValue();
-        String cachedValue = opsForValue.get(redisKey);
+        String cacheKey = String.format("yupicture:listPictureVOByPage:%s", hashKey);
+        //1. 先从本地缓存中查询
+        String cachedValue = LOCAL_CACHE.getIfPresent(cacheKey);
         if (cachedValue != null) {
             //如果缓存命中，缓存结果
             Page<PictureVO> cachedPage = JSONUtil.toBean(cachedValue, Page.class);
             return ResultUtils.success(cachedPage);
         }
-        // 查询数据库
+        //2. 本地缓存未命中，查询redis分布式缓存
+        ValueOperations<String, String> opsForValue = stringRedisTemplate.opsForValue();
+        cachedValue = opsForValue.get(cacheKey);
+        if (cachedValue != null) {
+            //如果redis缓存命中，更新本地缓存，缓存结果
+            LOCAL_CACHE.put(cacheKey, cachedValue);
+            Page<PictureVO> cachedPage = JSONUtil.toBean(cachedValue, Page.class);
+            return ResultUtils.success(cachedPage);
+        }
+        // 3. 本地缓存和redis缓存都没有命中，查询数据库
         Page<Picture> picturePage = pictureService.page(new Page<>(current, size),
                 pictureService.getQueryWrapper(pictureQueryRequest));
         Page<PictureVO> pictureVOPage = pictureService.getPictureVOPage(picturePage, request);
-        //存入redis缓存
+        //4. 更新缓存
+        //更新redis缓存
         String cacheValue = JSONUtil.toJsonStr(pictureVOPage);
         //设置缓存过期时间，5~10分钟过期，加入一些随机因素防止缓存雪崩
         int cacheExpireTime = 300 + RandomUtil.randomInt(0, 300);
-        opsForValue.set(redisKey, cacheValue, cacheExpireTime, TimeUnit.SECONDS);
+        //opsForValue.set(redisKey, cacheValue, cacheExpireTime, TimeUnit.SECONDS);
+        //更新本地缓存
+        LOCAL_CACHE.put(cacheKey, cacheValue);
         // 获取封装类
         return ResultUtils.success(pictureVOPage);
     }
